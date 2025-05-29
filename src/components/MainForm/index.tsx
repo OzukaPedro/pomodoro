3c99f920155292652210d6d688673e63119f04a1
Pedro Campagnoli <pedrocampagnoli28@gmail.com>
2025-05-29 14:04:11 -0300

    main form atualizado

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,16 +3,31 @@ import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 import styles from "./styles.module.css";
+import { useState } from "react";
 export function MainForm() {
+  const [taskName, setTaskName] = useState("");
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   return (
-    <form className={styles.mainForm}>
-      <DefaultInput
-        type="text"
-        id="MainInput"
-        placeholder="teste"
-        labelText="Task"
-      />
-      <DefaultButton icon={<PlayCircleIcon />} />
+    <form onSubmit={handleCreateNewTask} className={styles.mainForm}>
+      <div className="formRow">
+        <DefaultInput
+          type="text"
+          id="MainInput"
+          placeholder="teste"
+          labelText="Task"
+          value={taskName}
+          onChange={(e) => {
+            setTaskName(e.target.value);
+          }}
+        />
+      </div>
+      <div className="formRow">
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
     </form>
   );
 }
